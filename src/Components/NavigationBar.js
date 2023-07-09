79423569db76f8127372adda00ad2a8d619124b0
Christopher Gharghou <christophergargour@gmail.com>
2023-07-09 21:17:09 +0300

    Added correct links for navigation bar and bug fix

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import ewbLogo from "../Media/UCLAEWBLogo.png";
 import { Link } from "react-router-dom"; 
 import ReorderIcon from '@mui/icons-material/Reorder';
@@ -11,22 +11,47 @@ const toggleNavbar = () => {
   setOpenLinks(!openLinks);
 };
 
+//test
+
+const [isScreenLessThan720, setIsScreenLessThan720] = useState(false);
+
+useEffect(() => {
+  const handleResize = () => {
+    setIsScreenLessThan720(window.innerWidth < 720);
+  };
+
+  // Initial check
+  handleResize();
+
+  // Event listener for window resize
+  window.addEventListener('resize', handleResize);
+
+  // Cleanup the event listener on component unmount
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
+//test
+
   return (
     <div className="navigationbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
+      <div className="leftSide" id={openLinks && isScreenLessThan720 ? "open" : "close"}>
         <img src={ewbLogo} /> 
         <div className="hiddenLinks">
-          <Link to="/">Home</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+        <Link to="/aboutus">About us</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/joinus">Join us</Link>
+        <Link to="/donate">Donate</Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to="/">Home</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/about">About</Link>
+        <Link to="/aboutus">About us</Link>
+        <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/joinus">Join us</Link>
+        <Link to="/donate">Donate</Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
