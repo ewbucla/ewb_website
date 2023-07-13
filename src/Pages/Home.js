b25279a4e53571d2fcf4a3f0ebc120c7ef90e945
Christopher Gharghou <christophergargour@gmail.com>
2023-07-13 16:59:18 +0300

    Implemented basic elements of home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import BackgroundIMG from '../Media/bgpic.JPG'
+import '../CSS/Home.css'
 
 function Home() {
-  return <div> Hey </div>;
+  return (
+    <div className="home">
+      <div className="headerContainer" style={{ backgroundImage: `url(${BackgroundIMG})`}}>
+        <h1>Engineers Without Borders</h1>
+        <p> University of California, Los Angeles Student Chapter</p>
+        <Link to="https://support.ewb-usa.org/team/501623">
+          <button>DONATE</button>
+        </Link>
+      </div>
+    </div>
+  );
+
 }
 
 export default Home;
