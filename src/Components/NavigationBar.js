01afb547833cb0d32874094717df257156e2cbc4
Christopher Gharghou <christophergargour@gmail.com>
2023-08-07 19:37:16 +0300

    moved drop down navigation bar to top right

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import ReorderIcon from '@mui/icons-material/Reorder';
 import CloseIcon from '@mui/icons-material/Close';
 import '../CSS/NavigationBar.css';
+import { InsertDriveFile } from '@mui/icons-material';
 
 function NavigationBar() {
 
@@ -11,43 +12,44 @@ function NavigationBar() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+
   return (
     <>
-      <nav className='navigationbar'>
-      <img src={ewbLogo} />
+      <div className='navigationbar'>
+        <img src={ewbLogo} />
         <div className='menu-icon' onClick={handleClick} >
           <button onClick={handleClick}>
            {click ? <CloseIcon /> : <ReorderIcon />}
           </button>
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
+        <div className={click ? 'nav-menu active' : 'nav-menu'}>
+          <div className='nav-item'>
             <Link to='/' className='nav-links' onClick={closeMobileMenu}>
               Home
             </Link>
-          </li>
-          <li className='nav-item'>
+          </div>
+          <div className='nav-item'>
             <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}>
               About Us
             </Link>
-          </li>
-          <li className='nav-item'>
+          </div>
+          <div className='nav-item'>
             <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
               Projects
             </Link>
-          </li>
-          <li className='nav-item'>
+          </div>
+          <div className='nav-item'>
             <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
               Contact
             </Link>
-          </li>
-          <li className='nav-item'>
+          </div>
+          <div className='nav-item'>
             <Link to='/joinus' className='nav-links' onClick={closeMobileMenu}>
               Join Us
             </Link>
-          </li>
-        </ul>
-      </nav>
+          </div>
+        </div>
+      </div>
     </>
 
   );
