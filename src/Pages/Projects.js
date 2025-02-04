eeb29322773cef51cfa2eda7bbe57acbce779147
Arathi Nair <arathinair@g.ucla.edu>
2025-02-04 15:49:07 -0800

    remove EBW distinction

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,68 +1,61 @@
-import React from 'react';
-import { EWBList } from '../Helpers/EWBList';
-import { EBWList } from '../Helpers/EBWList';
-import { OldProjectsList } from '../Helpers/OldProjectsList';
-import ProjectItem from '../Components/ProjectItem';
-import ProjectsBG from '../Media/ProjectsBG.jpg';
-import { Link } from "react-router-dom"; 
+import React from "react";
+import { EWBList } from "../Helpers/EWBList";
+import { EBWList } from "../Helpers/EBWList";
+import { OldProjectsList } from "../Helpers/OldProjectsList";
+import ProjectItem from "../Components/ProjectItem";
+import ProjectsBG from "../Media/ProjectsBG.jpg";
+import { Link } from "react-router-dom";
 
 import "../CSS/Projects.css";
 
 function Projects() {
   return (
-    <div className="projects" >
+    <div className="projects">
       <div className="projectsTitleSection">
         <div className="projectsOverlay"></div>
         <div className="projectsTitle">Our Projects</div>
       </div>
-        <div className="CurrentProjects">Current Projects</div>
-        <div className="subtitle">Projects Under EWB</div>
-        <div className="projectsList">
-          {EWBList.map((projectItem, key) => {
-            return (
-            <Link className="projectLink" key={key} to={`/projects/${projectItem.id}`}>
-            <ProjectItem
-            key={key}
-            image={projectItem.image}
-            name={projectItem.name}
-            description={projectItem.description} 
-            />
+      <div className="CurrentProjects">Current Projects</div>
+      <div className="subtitle">Projects Under EWB</div>
+      <div className="projectsList">
+        {EWBList.map((projectItem, key) => {
+          return (
+            <Link
+              className="projectLink"
+              key={key}
+              to={`/projects/${projectItem.id}`}
+            >
+              <ProjectItem
+                key={key}
+                image={projectItem.image}
+                name={projectItem.name}
+                description={projectItem.description}
+              />
             </Link>
-            );
-          })}
-          </div>
-          <div className="subtitle">Projects Under EBW</div>
-          <div className="projectsList">
-          {EBWList.map((projectItem, key) => {
-            return (
-            <Link className="projectLink" key={key} to={`/projects/${projectItem.id}`}>
-            <ProjectItem
-            key={key}
-            image={projectItem.image}
-            name={projectItem.name}
-            description={projectItem.description} 
-            />
+          );
+        })}
+      </div>
+      <div className="OldProjects">Old Projects</div>
+      <div className="projectsList">
+        {OldProjectsList.map((projectItem, key) => {
+          return (
+            <Link
+              className="projectLink"
+              key={key}
+              to={`/projects/${projectItem.id}`}
+            >
+              <ProjectItem
+                key={key}
+                image={projectItem.image}
+                name={projectItem.name}
+                description={projectItem.description}
+              />
             </Link>
-            );
-          })}
-          </div>
-          <div className="OldProjects">Old Projects</div>
-          <div className="projectsList">
-          {OldProjectsList.map((projectItem, key) => {
-            return (
-            <Link className="projectLink" key={key} to={`/projects/${projectItem.id}`}>
-            <ProjectItem
-            key={key}
-            image={projectItem.image}
-            name={projectItem.name}
-            description={projectItem.description} 
-            />
-            </Link>
-            );
-          })}
-        </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-export default Projects
+export default Projects;
