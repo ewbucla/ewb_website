90295668d5da4c5be38f1fb2e6a2174cc6b8d7d5
Richard Amador <richardamador@RichardAmador2026.local>
2025-02-12 19:07:28 -0800

    make projects names appear on nav mouse hover

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 import ReorderIcon from '@mui/icons-material/Reorder';
 import CloseIcon from '@mui/icons-material/Close';
 import '../CSS/NavigationBar.css';
-import { InsertDriveFile } from '@mui/icons-material';
+// import { InsertDriveFile } from '@mui/icons-material';
+// import the project items for nav hover
+import { EWBList } from "../Helpers/EWBList";
 
 function NavigationBar() {
 
@@ -18,7 +20,7 @@ function NavigationBar() {
   return (
     <>
       <div className='navigationbar'>
-        <img src={ewbLogo} />
+        <img src={ewbLogo} alt='EWB Logo'/>
         <div className='menu-icon' onClick={handleClick} >
           <button onClick={handleClick}>
            {click ? <CloseIcon /> : <ReorderIcon />}
@@ -35,10 +37,21 @@ function NavigationBar() {
               About Us
             </Link>
           </div>
-          <div className='nav-item'>
-            <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+          <div className='nav-item projects'>
+            <Link to='/projects' className='nav-links projects-link' onClick={closeMobileMenu}>
               Projects
             </Link>
+            <div className="projects-dropdown-container">
+              {EWBList.map((item, index) => {
+                return(
+                  <div className="dropdown-item" key={index}>
+                    <Link to={"/projects/" + item.id} className="dropdown-item-link">
+                      {item.name}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
           </div>
           <div className='nav-item'>
             <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
