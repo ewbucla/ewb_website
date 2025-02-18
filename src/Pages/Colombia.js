18443184f198d02a1bbd865b903e8f98f224d65c
neham-04 <156238521+neham-04@users.noreply.github.com>
2025-02-17 23:09:40 -0800

    finished edits to colombia's page

--- src/Pages/Colombia.js
+++ src/Pages/Colombia.js
@@ -11,7 +11,7 @@ function Colombia() {
           <div className="colombiaTitle">Colombia</div>
           <div className="colombiaTitle2">Sanitation, Education & Development</div>
         </div>
-        <h1 className="mainTitle">Project Leads</h1>
+        <h1 className="mainTitle">Project Managers</h1>
         <div className="BoardList">
           {ColombiaBoard.map((projectItem, key) => {
             return (
@@ -29,7 +29,7 @@ function Colombia() {
         </div>
         <h1 className="sectionTitles">About the Project</h1>
         <p className="sectionParagraphs">The Colombia project consists of building 3 biofactories in the localities of Caqueta, Planadas, and Armenia which will each serve around 100 community members.</p>
-        <h1 className="sectionTitles">About Colombia</h1>
+        <h1 className="sectionTitles">About the Community</h1>
         <p className="sectionParagraphs">In 2022, 5 million Colombians did not reach their cognitive potential due to chronic malnutrition during childhood. Affected communities are thus not only underfed, they are also prevented from improving their quality of life by obtaining higher-paying jobs and left vulnerable as a result of undernourishment.</p>
         <h1 className="sectionTitles">Current State of Project</h1>
         <p className="sectionParagraphs"> We have finished our designs, drafted our construction drawings, and elaborated a bill of materials. Currently, we are awaiting final confirmation of the budget and drawings by the architects with whom we are working in Colombia.</p>
