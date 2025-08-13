6b2a89c28419897a2b100316cb50d94cca173945
ewbucla <ewbucla@gmail.com>
2025-08-12 20:02:06 -0700

    Update AltHome.js
    add USA to "engineers without borders" in header

--- src/Pages/AltHome.js
+++ src/Pages/AltHome.js
@@ -10,7 +10,7 @@ function Home() {
     <div className = "althome">
     <div className="homeewb" style={{ backgroundImage: `url(${BackgroundIMGEWB})` }}>
       <div className="headerContainerEWB">
-        <h1>Engineers Without Borders</h1>
+        <h1>Engineers Without Borders USA</h1>
         <p> University of California, Los Angeles Student Chapter</p>
         <Link to="https://support.ewb-usa.org/team/501623">
           <button>Learn more about EWB</button>
