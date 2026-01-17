a173bf69686f885db27f578aca718fb0f3ddb658
Ray Cheng <raycheng25456@gmail.com>
2026-01-17 15:12:00 -0800

    [IN PROG] Revised spacing in EcuadorBoard.js

--- src/Helpers/EcuadorBoard.js
+++ src/Helpers/EcuadorBoard.js
@@ -3,18 +3,17 @@ import AditiHeadshot from "../Media/AditiHeadshot.png";
 
 export const EcuadorBoard = [
     {
-        id: "alisha",
-        name: "Alisha",
-        image: AlishaHeadshot,
-        description: "3rd year",
-        major: "Bioengineering"
+     id: "alisha",
+     name: "Alisha",
+     image: AlishaHeadshot,
+     description: "3rd year",
+     major: "Bioengineering"
     },
     {
-        id: "aditi",
-        name: "Aditi",
-        image: AditiHeadshot,
-        description: "3rd year",
-        major: "Civil Engineering"
+     id: "aditi",
+     name: "Aditi",
+     image: AditiHeadshot,
+     description: "3rd year",
+     major: "Civil Engineering"
     },
-
-]
+];
