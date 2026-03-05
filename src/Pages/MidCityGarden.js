2bfa3186619aaa4044a84ca19034560d55de2720
ray25456 <ray25456@g.ucla.edu>
2026-03-04 18:04:09 -0800

    Added Mid City Garden js page

---
+++ src/Pages/MidCityGarden.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ProjectItem from '../Components/ProjectItem';
+import "../CSS/MidCityGarden.css";
+import { useEffect } from "react"; 
+
+function MidCityGarden() {
+    useEffect(() => {
+    window.scrollTo(0, 0); }, []);
+    return (
+        <div className="MidCityGarden">
+            <div className="MidCityGardenTitleSection">
+                <div className = "MidCityGardenOverlay"></div>
+                <div className="MidCityGardenTitle">MidCityGarden</div>
+                <div className="MidCityGardenTitle2">"Work in progress"</div>
+            </div>
+            <h1 className="sectionTitles">Project Leads</h1>
+            <div className="BoardList">
+                {MidCityGarden.map((projectItem, key) => {
+                    return (
+                    <div className="Boardmember" key={key} to={`/projects/${projectItem.id}`}>
+                    <ProjectItem
+                    key={key}
+                    image={projectItem.image}
+                    name={projectItem.name}
+                    description={projectItem.description}
+                    major={projectItem.major}
+                    />
+                    </div>
+                    );
+                })}
+                </div>
+
+
+
+    </div>
+    )
+}
+
+export default MidCityGarden
