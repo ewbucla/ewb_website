c8c4f1b7f988213e214fc22ac2ee7a26ea0b49cf
ray25456 <ray25456@g.ucla.edu>
2026-03-04 19:56:20 -0800

    Fixed MidCityGarden.js

--- src/Pages/MidCityGarden.js
+++ src/Pages/MidCityGarden.js
@@ -2,27 +2,35 @@ import React from 'react'
 import { MidCityGardenBoard } from '../Helpers/MidCityBoard';
 import ProjectItem from '../Components/ProjectItem';
 import "../CSS/MidCityGarden.css";
-import { useEffect } from "react"; 
+import { useEffect } from "react";
 
 function MidCityGarden() {
-    useEffect(() => {
-    window.scrollTo(0, 0); }, []);
-    return (
-        <div className="MidCityGarden">
-            <div className="MidCityGardenTitleSection">
-                <div className = "MidCityGardenOverlay"></div>
-                <div className="MidCityGardenTitle">MidCityGarden</div>
-                <div className="MidCityGardenTitle2">"New Project Coming Soon."</div>
+  useEffect(() => {
+  window.scrollTo(0, 0); } , []);
+  return (
+    <div className="ecuador"> 
+        <div className="ecuadorTitleSection">
+          <div className="ecuadorOverlay"></div>
+          <div className="ecuadorTitle">Ecuador</div>
+          <div className="ecuadorTitle2">Improving access to safe, high-quality water in San Pedro de Cachihuayco, Ecuador</div>
+        </div>
+        <h1 className="sectionTitles">Project Leads</h1>
+        <div className="BoardList">
+          {MidCityGardenBoard.map((projectItem, key) => {
+            return (
+            <div className="Boardmember" key={key} to={`/projects/${projectItem.id}`}>
+            <ProjectItem
+            key={key}
+            image={projectItem.image}
+            name={projectItem.name}
+            />
             </div>
-            <h1 className="sectionTitles">Project Leads</h1>
-            <div className="BoardList">
-                
-                </div>
-
-
+            );
+          })}
+        </div>
 
     </div>
-    )
-}
+        )
+    }
 
 export default MidCityGarden
